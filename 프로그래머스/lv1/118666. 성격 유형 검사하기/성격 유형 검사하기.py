cc488678419c1f7618edd327bab8203cee31eb7e
고희석 / heeseok Go <96563183+GoHeeSeok00@users.noreply.github.com>
2022-09-15 12:18:04 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -2,7 +2,7 @@ def solution(survey, choices):
     answer = ""
     indicators = [["R", "T"], ["C", "F"], ["J", "M"], ["A", "N"]]
     table = {1: [0, 3], 2: [0, 2], 3: [0, 1], 5: [1, 1], 6: [1, 2], 7: [1, 3]}
-    rank = {} # {"R": 0, "T": 0, "C": 0,  "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
+    rank = {} # {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
     
     for i in indicators:
         rank.update({i[0]: 0, i[1]: 0})
