6624e466029e0618626136742df3837aaa371ecc
고희석 / heeseok Go <96563183+GoHeeSeok00@users.noreply.github.com>
2023-07-14 13:56:08 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.py
@@ -1,11 +1,8 @@
 def solution(x):
-    answer = True
     str_number = str(x)
     sum_number = 0
     
     for sn in str_number:
         sum_number += int(sn)
-    
-    if x % sum_number != 0:
-        answer = False
-    return answer
+
+    return x % sum_number == 0
