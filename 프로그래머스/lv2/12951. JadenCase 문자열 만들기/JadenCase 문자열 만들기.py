2019dc5f00c5197d447c57d5a896577d2f175bf0
고희석 / heeseok Go <96563183+GoHeeSeok00@users.noreply.github.com>
2023-10-06 15:49:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,10 +1,4 @@
-def isAlphaToUpperStr(word):
-    if word.isalpha():
-        return word.upper()
-    return word
-
 def solution(s):
-    # 방법 1. 문자열을 공백 기준으로 나누고(배열에 단어별로) 각 단어의 첫 문자열이 숫자가 아닌경우 JadenCase, 숫자인 경우는 LowerCase수정
     answer = []
     words = s.split(' ')
     print(words)
@@ -16,13 +10,3 @@ def solution(s):
         else:
             answer.append(word.lower())
     return " ".join(answer)
-    # 방법 2. 모든 문자열을 소문자로 변경, 문자열 반복 시 첫 문자와 공백 이후에 오는 문자 숫자여부에 따른 대문자 변경
-    # answer = ''
-    # for i, j in  enumerate(s.lower()):
-    #     if i == 0:
-    #         answer += isAlphaToUpperStr(j)
-    #     elif j.isalpha() and s[i-1] == ' ':
-    #         answer += isAlphaToUpperStr(j)
-    #     else:
-    #         answer += j
-    # return answer
