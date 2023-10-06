593fee60436d08ff51d8d90dc3337d2a542244f1
고희석 / heeseok Go <96563183+GoHeeSeok00@users.noreply.github.com>
2023-10-06 15:50:33 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,14 +1,14 @@
 function solution(s) {
     const answer = [];
-    const words = s.split(' ');
+    const words = s.toLowerCase().split(' ');
     console.log(words);
     for (const word of words) {
         if (word === '') {
             answer.push(word);
         } else if (isNaN(word[0])) {
-            answer.push(word[0].toUpperCase() + word.slice(1).toLowerCase());
+            answer.push(word[0].toUpperCase() + word.slice(1));
         } else {
-            answer.push(word.toLowerCase());
+            answer.push(word);
         }
             
         console.log(word);
